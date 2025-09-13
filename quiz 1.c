07ba09fc8488673e954a52097f54868e17afcf35
keviniankiplagat-ux <keviniankiplagat@gmail.com>
2025-09-13 11:24:52 +0300

    Add files via upload

---
+++ quiz 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    printf("SPM2100 Discrete mathematics\n");
+    printf("SPM2104 Mathematics for science\n");
+    printf("UCU2105 Fundmental of ICT\n");
+    printf("SPI2102 Computer system and organization\n");
+    printf("UCU2101 Communication skills\n");
+       printf("SPC2101 Introduction t0 program and programming solving\n");
+    printf("SSE2101 Fundermental of system analysis and design\n");
+ 
+    return 0;
+}
