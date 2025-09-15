b6e69d73ef68722d4eec77e2c0d39eb903000df9
keviniankiplagat-ux <keviniankiplagat@gmail.com>
2025-09-15 21:11:23 +0300

    Add files via upload

---
+++ QUIZ 2.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){ 
+	int id_number;
+	float bank_balance,height;
+	char email;
+	printf("enter the height:");
+	scanf("%f", &height);
+	printf("enter the bank_balance:");
+	scanf("%f",&bank_balance);
+	printf("enter the id_number:");
+	scanf("%d", &id_number);
+	printf("enter the email:");
+	scanf("%s",email);
+	
+
+	printf("height:%f\n",height);
+	printf("bank_balance:$f\n",bank_balance);
+	printf("id_number:%d\n",id_number);
+	printf("email:%s\n", email);
+	return 0;
+}
+	
