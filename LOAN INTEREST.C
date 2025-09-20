5c6eb81ed10b4ebbc606a8d876d2a6b219baa03a
keviniankiplagat-ux <keviniankiplagat@gmail.com>
2025-09-20 10:29:53 +0300

    Add files via upload

---
+++ LOAN INTEREST.C
@@ -0,0 +1,16 @@
+# include<stdio.h>
+int main(){
+int age;	
+float salary;
+printf("enter the customer age:");
+scanf("%d",&age);
+printf("enter the customer salary:");
+scanf("%f",&salary);
+if(age>=21,salary>=21000){
+printf("congratulations you qualify for the loan");
+}
+else{
+printf("unfortunately we are not able to offer you loan at this time");
+}
+return 0;
+}
---
+++ volume and surface area.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>  
+int main() {
+    float radius, height, volume, surfaceArea;
+    const PI=3.142;
+
+    printf("Enter the radius of the cylinder: ");
+    scanf("%f", &radius);
+
+    printf("Enter the height of the cylinder: ");
+    scanf("%f", &height);
+
+
+    volume = PI * radius * radius * height;
+    surfaceArea = 2 * PI * radius * radius + 2 * PI * radius * height;
+
+    printf("Volume of the cylinder = %.2f\n", volume);
+    printf("Surface Area of the cylinder = %.2f\n", surfaceArea);
+
+    return 0;
+}
